891f7373e6522d42d8ab190f038bb8a975686e5f
JyotiRanjan1340 <j.r.badatya@gmail.com>
2025-11-27 17:26:13 +0530

    Add Rollup input configuration for multiple pages
    Configured Rollup options for multiple HTML entry points.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  plugins: [react()],
-});
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        team: resolve(__dirname, 'team.html'),
+        products: resolve(__dirname, 'products.html'),
+        'why-us': resolve(__dirname, 'why-us.html')
+      }
+    }
+  }
+})
