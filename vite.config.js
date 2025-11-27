48c9b6dc60cc607b11b625bd85840adb5f50920c
JyotiRanjan1340 <j.r.badatya@gmail.com>
2025-11-27 17:46:54 +0530

    Update Vite config for new directory structure

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,17 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
 
 export default defineConfig({
+  root: 'public',
   build: {
+    outDir: '../dist',
+    emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        about: resolve(__dirname, 'about.html'),
-        team: resolve(__dirname, 'team.html'),
-        products: resolve(__dirname, 'products.html'),
-        'why-us': resolve(__dirname, 'why-us.html')
+        main: 'public/index.html',
+        about: 'public/about.html',
+        'why-us': 'public/why-us.html',
+        team: 'public/team.html',
+        products: 'public/products.html'
       }
     }
   }
